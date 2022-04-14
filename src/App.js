78559ced3cd6f67e22dffdd4bf9bff9644fb8020
Lissana <liss.glace471@gmail.com>
2022-04-14 23:53:25 +0900

    feat: TodoアプリにTodoアイテムを追加する機能を追加
    ・Todoアイテムのタイトルを取得
    ・form要素のsubmitイベント（送信）を監視
    ・入力内容をタイトルにしたTodoアイテムを作成し、Todoリスト（#js-todo-list）にTodoアイテム要素を追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 console.log("App.js: loaded");
-export class App{
-    constructor(){
+
+import {element, render} from "./view/html-util.js";
+
+export class App {
+    constructor() {
         console.log("App initialized");
+    }
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElemnt = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCoutElement = document.querySelector("#js-todo-count");
+        
+        // TodoリストをまとめるList要素
+        const todoListElement = element`<ul />`;
+        // Todoアイテム数
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", (event) => {
+            // 本来のsubmitイベントの動作（他のURIに遷移。指定がない場合は自分自身）を止める
+            event.preventDefault();
+            
+            // 追加するTodoアイテムの要素(li要素)を作成する
+            const todoItemElement = element`<li>${inputElemnt.value}</li>`;
+
+            // TodoアイテムをtodoListElementに追加する
+            todoListElement.appendChild(todoItemElement);
+
+            // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+            render(todoListElement, containerElement);
+
+            // Todoアイテム数を+1し、表示されているテキストを更新する
+            todoItemCount += 1;
+            todoItemCoutElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+
+            // 入力欄を空文字列にしてリセットする
+            inputElemnt.value = "";
+        });
+
     }
 }
